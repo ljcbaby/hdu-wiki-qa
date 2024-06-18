31f3cb501a33239d4c33e124b89ab41d7b6fab97
Ljcbaby <46277145+ljcbaby@users.noreply.github.com>
2024-06-18 21:22:17 +0800

    chore: Add CORS middleware to enable cross-origin resource sharing
    为了实现跨域资源共享，添加了CORS中间件

--- service/service.go
+++ service/service.go
@@ -29,6 +29,7 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.Use(LoggerMiddleware())
+	r.Use(CORS())
 
 	r.GET("/", Index)
 	r.POST("/v1/chat/completions", Chat)
@@ -40,4 +41,21 @@ func Index(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Hello, HDU Wiki QA!",
 	})
+}
+
+func CORS() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "*")
+		ctx.Header("Access-Control-Allow-Headers", "*")
+		ctx.Header("Access-Control-Expose-Headers", "*")
+		ctx.Header("Access-Control-Allow-Credentials", "true")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		ctx.Next()
+	}
 }
